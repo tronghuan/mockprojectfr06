e1c69e88131250b3c1a5bcf4156224ea4eb09a5c
thinhld2408 <thinhld@smartosc.com>
2014-08-19 09:39:57 +0700

    Delete Brand
    Up code delete Brand (chưa test)

--- application/modules/admin/controllers/brand.php
+++ application/modules/admin/controllers/brand.php
@@ -5,3 +5,23 @@
  * Date: 8/18/14
  * Time: 1:54 PM
  */
+class Brand extends AdminBaseController{
+    function __construct(){
+        parent::__construct();
+        $this->load->helper("url");
+
+    }
+    public function index(){
+
+    }
+    public function delete(){
+        $id = $this->uri->segment(4);
+        $this->brand_model->del($id);
+        redirect(base_url()."admin/user/listbrand");
+        exit();
+    }
+    public function update(){
+
+    }
+
+}
