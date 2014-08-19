c3eb8baa870b87126a15560a91a82cce882c4e7e
thinhld2408 <thinhld@smartosc.com>
2014-08-19 09:25:12 +0700

    Revert "huhhuhu"
    This reverts commit d4e1caf553177375c1ec8adbe9bfe4792c74dd8c.

--- application/modules/admin/controllers/hghg.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: CITA
- * Date: 19/08/14
- * Time: 09:22
- */ ghghghg
--- application/modules/admin/controllers/user.php
+++ application/modules/admin/controllers/user.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: TrongHua
+ * User: TrongHuan
  * Date: 8/18/14
  * Time: 1:54 PM
  */
