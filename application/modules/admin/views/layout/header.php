693daabeffafa8be222c3f4e96e0f7002a999804
tronghuan <huandt92@gmail.com>
2014-08-19 10:42:13 +0700

    chinh code search va login logout

--- application/modules/admin/views/layout/header.php
+++ application/modules/admin/views/layout/header.php
@@ -13,16 +13,25 @@
             <div class="category">
                 <h1>MENU CHỨC NĂNG</h1>
                 <ul>
-                    <li><a href="<?php echo base_url(); ?>index.php?module=admin&controller=user&action=index">Quản
+                    <li><a href="<?php echo base_url(); ?>index.php/admin/user/listuser">Quản
                             lý thành viên</a></li>
                     <li><a href="#">Quản lý chuyên mục</a></li>
                     <li><a href="#">Quản lý sản phẩm</a></li>
-                    <li><a href="#">Quản lý nhãn hiệu</a></li>
+                    <li><a href="<?php echo base_url(); ?>index.php/admin/brand/search">Quản lý nhãn hiệu</a></li>
                     <li><a href="#">Quản lý đơn hàng </a></li>
                     <li><a href="#">Quản lý cấu hình </a></li>
                     <li><a href="#">Quản lý comment </a></li>
                 </ul>
             </div>
-            <div class="category"></div>
+            <div class="category" style="margin-top:10px; border-top:1px solid #CCC; background:#FFF;">
+                <p>Chào bạn:
+                    <?php if(session_id() == '') {
+                        session_start();
+                    };
+                    // print_r($_SESSION['user']);
+                    echo  isset($_SESSION['user']['username']) ?  $_SESSION['user']['username'] : "";
+                    ?></p>
+                <p><a href=' <?php echo base_url("index.php/admin/user/login") ?> '>Đăng xuất</a></p>
+            </div>
 
         </div>
