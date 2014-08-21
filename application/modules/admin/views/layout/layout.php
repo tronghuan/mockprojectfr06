a31d84f8510f9c65707fcbe3b0bef071d26f3e5e
tronghuan <huandt92@gmail.com>
2014-08-21 09:24:02 +0700

    Hoan thành công việc Day 2 Move Category

---
+++ application/modules/admin/views/layout/layout.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TrongHuan
+ * Date: 8/20/14
+ * Time: 9:30 AM
+ */
+$this->load->view("layout/header");
+$this->load->view($template);
+$this->load->view("layout/footer");
