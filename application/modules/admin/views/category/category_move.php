0626808d59a9fd7393151b53fabe57a2ab918a1c
tronghuan <huandt92@gmail.com>
2014-08-21 16:08:32 +0700

    Sua loi nhỏ khi move category cho hoàn thành

--- application/modules/admin/views/category/category_move.php
+++ application/modules/admin/views/category/category_move.php
@@ -7,20 +7,9 @@
  * Time: 11:31 PM
  */
 echo "<div class='dd' id='catetree'>";?>
-    <div class="cf nestable-lists">
-
-        <div class="dd" id="nestable">
 
 <?php
 echo $listitem;
 ?>
-        </div>
-        <p><strong>Serialised Output (per list)</strong></p>
-
-        <textarea id="nestable-output"></textarea>
-
-        <p>&nbsp;</p>
-    </div>
 </div>
-
 </div>
--- public/js/admin/js.js
+++ public/js/admin/js.js
@@ -1,8 +1,44 @@
 /**
- * 
+ *
  */
 
-$(function(){
+$(function () {
+    $("#catetree").nestable({
+        group: 1
+    })
+        .on("change", function () {
+            var data = $('#catetree').nestable("serialize");
+            var $json = createOut(data, 0, 0);
+            $json = JSON.parse("{" + $json.slice(0, $json.length - 1) + "}");
+            $.ajax({
+                type: "POST",
+                url: "move_process",
+                data: {data: $json}
+            });
+        });
+
+    function createOut(data, parent, level) {
+        var out = "";
+        var i;
+        if (!parent) {
+            parent = 0;
+        }
+        if (!level) {
+            level = 0;
+        }
+        for (i = 0; i < data.length; i++) {
+            out += '"' + data[i]['id'] + 'a":"' + parent + '",';
+            if ((typeof data[i]['children']) == "object") {
+                out += createOut(data[i]['children'], data[i]['id'], level = 1);
+            }
+        }
+        return out;
+    }
+});
+
+/**
+
+ $(function(){
 	$("#catetree").nestable({
 		group:1
 	})
@@ -36,3 +72,5 @@ $(function(){
 		return out;
 	}
 });
+
+ */
